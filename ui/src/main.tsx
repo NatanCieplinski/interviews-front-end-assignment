99eab8f9df297fee62a41c8ffb25c4edd08cadcc
NatanCieplinski <natan.cieplinski.developer@gmail.com>
2024-06-28 00:18:07 +0200

    Config react query and react router

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,15 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router-dom'
+import { queryClient } from './config/react-query.ts'
+import { router } from './config/react-router.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
